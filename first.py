1eafa70e0b3147be2fc7bf8d7812a8c5808b0812
Max45515 <maksimsteblyakov@gmail.com>
2026-03-31 16:06:00 +0200

    Add simple descending sort implementation
    Implement a simple sorting algorithm to sort numbers in descending order.

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
+# Простая сортировка (попросил ИИ проверить на ошибки (в целом код рабочий но его можно улучшить) получилось то что ниже)
 
+arr = list(map(int, input("Введите числа через пробел: ").split()))
+
+n = len(arr)
+
+for j in range(n):
+    for i in range(n - 1):
+        if arr[i] < arr[i + 1]:
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+
+print(arr)
